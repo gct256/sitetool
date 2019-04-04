4ce38fb65f21a00be81d6cf03dadb9842a3c02dd
gct256 <gct256+github@gmail.com>
2019-04-04 15:57:14 +0900

    add html-format options

--- src/funcs/htmlFormat.ts
+++ src/funcs/htmlFormat.ts
@@ -3,19 +3,41 @@ import prettier from 'prettier';
 import { BuildContainer } from '../core/Builder';
 import { Target } from '../core/Target';
 
+interface HtmlFormatOptions {
+  beautify?: boolean;
+}
+
+const developmentOptions: HtmlFormatOptions = {
+  beautify: true
+};
+
+const productionOptions: HtmlFormatOptions = {
+  beautify: true
+};
+
 export async function htmlFormat(
   container: BuildContainer,
   target: Target
 ): Promise<BuildContainer> {
-  if (!target.distribute) return container;
-
-  const result: string = prettier.format(container.buffer.toString('utf8'), {
-    parser: 'html'
-  });
-
-  return {
-    buffer: Buffer.from(result, 'utf8'),
-    sourceMap: container.sourceMap,
-    hasError: false
+  const options: HtmlFormatOptions = {
+    ...(target.distribute ? productionOptions : developmentOptions),
+    ...target.config.getOption<HtmlFormatOptions>(
+      'html-format',
+      target.distribute
+    )
   };
+
+  if (options.beautify) {
+    const result: string = prettier.format(container.buffer.toString('utf8'), {
+      parser: 'html'
+    });
+
+    return {
+      buffer: Buffer.from(result, 'utf8'),
+      sourceMap: container.sourceMap,
+      hasError: false
+    };
+  }
+
+  return container;
 }
